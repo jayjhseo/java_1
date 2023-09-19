eeb6c51b08b9f09a6b1842e48f09ab09fc3d0ecd
깃jayjhseo <jayjhseo@gmail.com>
2023-09-19 11:08:26 +0900

    test_05

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,35 +1,51 @@
 package org.example;
 
 
+import com.sun.jdi.PathSearchingVirtualMachine;
+
 class Main {
     public static void main(String[] args) {
-        // `int a` => a라는 이름의 변수(즉 공간)을 만든다.
-        // `int` => a라는 공간에는 정수만 담을 수 있다.
-        // `;` => 문장이 끝남을 알린다.
-        int a;
-        a = 5;
+        int i = 1;
+        while (i <= 10) {
+            int j = 1;
+            while (j <= 3) {
+                System.out.println(j);
+                j++;
+            }
+            i++;
+        }
 
-        System.out.println(a);
-        System.out.println(a + 10);
 
-        int b = 10;
-
-        // `+` => 문장과 문자을 합친다.
-        System.out.println("a : " + a);
-        System.out.println("b : " + b);
-
-        // 여기서 부터
-//      a = a + 5;
-//      b = a - 5;
-        int temp = a;
-        a = b;
-        b = temp;
-        // 여기까지 수정 가능
-
-        System.out.println("a : " + a);
-        // 출력 : a : 10
-        System.out.println("b : " + b);
-        // 출력 :  b : 5
+        // 출력
+        // 1
+        // 2
+        // 3
+        // 1
+        // 2
+        // 3
+        // 1
+        // 2
+        // 3
+        // 1
+        // 2
+        // 3
+        // 1
+        // 2
+        // 3
+        // 1
+        // 2
+        // 3
+        // 1
+        // 2
+        // 3
+        // 1
+        // 2
+        // 3
+        // 1
+        // 2
+        // 3
+        // 1
+        // 2
+        // 3
     }
 }
-
