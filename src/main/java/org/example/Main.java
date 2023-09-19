234ae9c1e685f1d363014566657d1d50e1a33b05
깃jayjhseo <jayjhseo@gmail.com>
2023-09-19 11:51:45 +0900

    test_05

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,47 +5,19 @@ import com.sun.jdi.PathSearchingVirtualMachine;
 
 class Main {
     public static void main(String[] args) {
-        int i = 1;
-        while (i <= 10) {
-            int j = 1;
-            while (j <= 3) {
-                System.out.println(j);
-                j++;
-            }
-            i++;
+        int[] arr = {10, 20, 30};
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++) {
+            sum+= arr[i];
         }
+		/*
+		// 아래 4줄은 위 1줄과 같은 의미 입니다.
+		int[] arr = new int[3];
+		arr[0] = 10;
+		arr[0] = 20;
+		arr[0] = 30;
+		*/
 
-
-        // 출력
-        // 1
-        // 2
-        // 3
-        // 1
-        // 2
-        // 3
-        // 1
-        // 2
-        // 3
-        // 1
-        // 2
-        // 3
-        // 1
-        // 2
-        // 3
-        // 1
-        // 2
-        // 3
-        // 1
-        // 2
-        // 3
-        // 1
-        // 2
-        // 3
-        // 1
-        // 2
-        // 3
-        // 1
-        // 2
-        // 3
+        System.out.println(sum / arr.length);
     }
 }
